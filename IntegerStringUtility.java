e592db9e85ee7e3919536cdc55903e5a7d996989
yyddl <77661137+yyddl@users.noreply.github.com>
2026-09-20 16:41:29 -0600

    Update IntegerStringUtility.java
    Implement insertionSort and findMax

--- IntegerStringUtility.java
+++ IntegerStringUtility.java
@@ -14,11 +14,25 @@ public class IntegerStringUtility {
 	}
 
 	public static <E> void insertionSort(E[], Comparator<? super E>) {
-		
+		 for (int i = 1; i < array.length; i++) {
+            E current = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && comparator.compare(array[j], current) > 0) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = current;
+        }
 	}
 	
 	public static <E> E findMax(E[], Comparator<? super E>) {
-		
+		E[] copy = array.clone();
+
+        insertionSort(copy, comparator);
+
+        return copy[copy.length - 1];
 	}
 	
 	public static String[][] getSimilarityGroups(String[]) {
