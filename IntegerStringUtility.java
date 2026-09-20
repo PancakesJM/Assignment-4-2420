b257d0370ae5fa0ae2a458173e682bd1f9e15ed3
PancakesJM <u1498328@umail.utah.edu>
2026-09-20 13:51:01 -0600

    Update IntegerStringUtility.java
    Add class and method headers

--- IntegerStringUtility.java
+++ IntegerStringUtility.java
@@ -1,5 +1,31 @@
 package assign04;
 
 public class IntegerStringUtility {
+  public static class StringNumericalValueComparator implements Comparator<String> {
+		
+	}
+	
+	public static class StringSimilarityComparator implements Comparator<String> {
+		
+	}
+	
+	public static class StringSimilarityGroupComparator implements Comparator<String[]> {
+		
+	}
 
+	public static <E> void insertionSort(E[], Comparator<? super E>) {
+		
+	}
+	
+	public static <E> E findMax(E[], Comparator<? super E>) {
+		
+	}
+	
+	public static String[][] getSimilarityGroups(String[]) {
+		
+	}
+	
+	public static String[] findMaximumSimilarityGroup(int[]) {
+		
+	}
 }
