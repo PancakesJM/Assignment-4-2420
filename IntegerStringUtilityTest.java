cd50927ca34f79cb4cb9a86c0b8d3edb2cfe4bd5
PancakesJM <u1498328@umail.utah.edu>
2026-09-22 23:04:29 -0600

    Update IntegerStringUtilityTest.java
    Added a test for numerical comparator

--- IntegerStringUtilityTest.java
+++ IntegerStringUtilityTest.java
@@ -9,6 +9,8 @@ import java.util.Random;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import assign04.IntegerStringUtility.StringNumericalValueComparator;
+
 /**
  * Test class for IntegerStringUtility
  * 
@@ -17,9 +19,11 @@ import org.junit.jupiter.api.Test;
  */
 public class IntegerStringUtilityTest {
 	Integer[] intArray;
+	String[] stringArray;
 	Random random = new Random();
 	int randomInt;
-	Comparator<Integer> numerical;
+	Comparator<Integer> simpleNumerical;
+	StringNumericalValueComparator numerical;
 	@BeforeEach
 	void setUp() throws Exception {
 		intArray = new Integer[20];
@@ -27,26 +31,38 @@ public class IntegerStringUtilityTest {
 			randomInt = random.nextInt(20);
 			intArray[i] = randomInt;
 		}
-		numerical = new Comparator<Integer>() {
+		simpleNumerical = new Comparator<Integer>() {
 		    @Override
 		    public int compare(Integer num1, Integer num2) {
 		        return num1.compareTo(num2);
 		    }
 		};
+		numerical = new StringNumericalValueComparator();
+		stringArray = new String[4];
+		stringArray[0] = "149";
+		stringArray[1] = "024";
+		stringArray[2] = "1578";
+		stringArray[3] = "2354";
 	}
 	
 	@Test
 	public void insertionSortReturnsNumericalSort(){
-		IntegerStringUtility.insertionSort(intArray, numerical);
-		assertTrue(numerical.compare(intArray[0], intArray[1]) < 0);
+		IntegerStringUtility.insertionSort(intArray, simpleNumerical);
+		assertTrue(simpleNumerical.compare(intArray[0], intArray[1]) < 0);
 		System.out.print(Arrays.toString(intArray));
 	}
 	
 	@Test
 	public void maxReturnsBiggestNum() {
-		Integer maxNum = IntegerStringUtility.findMax(intArray, numerical);
+		Integer maxNum = IntegerStringUtility.findMax(intArray, simpleNumerical);
 		for(int i = intArray.length -1; i <= 0; i--)
-			assertTrue(numerical.compare(maxNum, i) > 0);
+			assertTrue(simpleNumerical.compare(maxNum, i) > 0);
 	}
 	
+	@Test
+	public void StringNumericalValueComparatorHasCorrectOrder(){
+		IntegerStringUtility.insertionSort(stringArray, numerical);
+		System.out.println(Arrays.toString(stringArray));
+	}
 }
+
